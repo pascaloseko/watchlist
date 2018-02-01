891fdc51c8d0e21f8a37f55100532902c1619b9d
cospamental <cospa18@gmail.com>
2018-02-01 20:58:28 +0300

    exported mail username and password

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 export MOVIE_API_KEY='9a6926a6b7db3149de3575019e44e932'
 export SECRET_KEY='8gdgdgdfdfdf9hshdhshdg6hdhdhdh44oo5'
 
+export MAIL_USERNAME='cospa18@gmail.com'
+export MAIL_PASSWORD='Pasco#mental21'
+
 python3.6 manage.py server
